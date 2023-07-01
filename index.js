6645018dfc444ce135167c1e5c591190fa83e1e1
misk <misksawallha@gmail.com>
2023-07-02 00:22:01 +0300

    display function

---
+++ index.js
@@ -0,0 +1,23 @@
+let inputBtn = document.getElementById("input-btn");
+let inputEL = document.getElementById("input-el");
+let ulEL = document.getElementById("ul-el")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+
+inputBtn.onclick = () => {
+    let inputValue = inputEL.value;
+    myLeads.push(inputValue);
+    console.log(myLeads);
+    display()
+};
+
+function display() {
+    let res = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        res += ` <li>${myLeads[i]}</li>`
+    }
+    ulEL.innerHTML = res
+    inputEL.value=" "
+}
+
+
+
