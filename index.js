43d688a55071965e6aa901bcb7c00f52ce9b7abd
misk <misksawallha@gmail.com>
2023-07-02 09:23:02 +0300

    delete button

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ let inputEL = document.getElementById("input-el");
 let ulEL = document.getElementById("ul-el")
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    display()
+}
+
 inputBtn.onclick = () => {
     let inputValue = inputEL.value;
     myLeads.push(inputValue);
-    console.log(myLeads);
+    //console.log(myLeads);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     display()
+    //console.log( localStorage.getItem("myLeads") )
 };
 
 function display() {
