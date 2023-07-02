77d1aece5ae4928bf673dc3a7ccb496f52dc8d33
misk <misksawallha@gmail.com>
2023-07-02 12:45:57 +0300

    save tab

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
-let inputBtn = document.getElementById("input-btn");
-let inputEL = document.getElementById("input-el");
-let ulEL = document.getElementById("ul-el")
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
-
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-console.log(leadsFromLocalStorage)
-
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    display()
+    render(myLeads)
 }
 
-inputBtn.onclick = () => {
-    let inputValue = inputEL.value;
-    myLeads.push(inputValue);
-    //console.log(myLeads);
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    display()
-    //console.log( localStorage.getItem("myLeads") )
-};
-
-function display() {
-    let res = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        res += ` <li><a target='_blank' href=${myLeads[i]}>${myLeads[i]}</a></li>`
+    render(myLeads)
+})
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
     }
-    ulEL.innerHTML = res
-    inputEL.value=" "
+    ulEl.innerHTML = listItems
 }
-
-
-
